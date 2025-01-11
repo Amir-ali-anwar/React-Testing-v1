e40fc503310d9d34ffcc386beebf2a2495113cc6
Amir Ali Anwar <amiralianwar611@gmail.com>
2025-01-11 23:16:06 +0500

    Setting up the Forn component for testing

--- src/05-form-testing/Sandbox.tsx
+++ src/05-form-testing/Sandbox.tsx
@@ -1,42 +1,86 @@
 
+import { useState } from "react";
+import validator from 'validator'
 const labelStyles = 'block text-grey-700 font-medium mb-2';
 const inputStyles = 'w-full px-3 py-2 border border-gray-300 rounded-md';
 const buttonsStyles =
   'w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600';
-
+const defaultState = {
+  email: '',
+  password: '',
+  confirmPassword: '',
+}
+type FormType = {
+  email: string | null;
+  password: string | null;
+  confirmPassword: string | null;
+};
 const Sandbox = () => {
+  const [fromInput, SetFormInput] = useState<FormType>(defaultState)
+  const [error, setError] = useState('');
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    SetFormInput({ ...fromInput, [id]: value });
+  };
+  const handleSubmit=(e:React.FormEvent<HTMLFormElement>)=>{
+    e.preventDefault();
+    if (!fromInput.email || !validator.isEmail(fromInput?.email)) {
+      return setError('Invalid email');
+    }
+    if (!fromInput.password || !validator.isLength(fromInput.password, { min: 5 })) {
+      return setError('Password must be at least 5 characters');
+    }
+    if (fromInput.password !== fromInput.confirmPassword) {
+      console.log(fromInput.password);
+      console.log(fromInput.confirmPassword);
+      
+      return setError('Passwords do not match');
+    }
+  console.log("clicked");
+  
+  }
   return <div className='container mx-auto max-w-md mt-10 p-6 bg-white rounded-lg shadow-md'>
-    <form className='space-y-4'>
-    <div className='mb-3'>
-          <label htmlFor='email' className={labelStyles}>
-            Email address
-          </label>
-          <input
-            type='email'
-            id='email'
-            className={inputStyles}
+    <form className='space-y-4' onSubmit={handleSubmit}>
+      <div className='mb-3'>
+        <label htmlFor='email' className={labelStyles}>
+          Email address
+        </label>
+        <input
+          type='email'
+          id='email'
+          className={inputStyles}
+          onChange={handleChange}
+          value={fromInput.email ?? ''}
           />
-        </div>
-        <div className='mb-3'>
-          <label htmlFor='email' className={labelStyles}>
+      </div>
+      <div className='mb-3'>
+        <label htmlFor='email' className={labelStyles}>
           Password
-          </label>
-          <input
-             type='password'
-            id='password'
-            className={inputStyles}
-          />
-        </div>
-        <div className='mb-3'>
-          <label htmlFor='confirmPassword' className={labelStyles}>
-            Confirm Password
-          </label>
-          <input
-            type='password'
-            id='confirmPassword'
-            className={inputStyles}
-          />
-        </div>
+        </label>
+        <input
+          type='password'
+          id='password'
+          className={inputStyles}
+          onChange={handleChange}
+          value={fromInput.password ?? ''}
+        />
+      </div>
+      <div className='mb-3'>
+        <label htmlFor='confirmPassword' className={labelStyles}>
+          Confirm Password
+        </label>
+        <input
+          type='password'
+          id='confirmPassword'
+          className={inputStyles}
+          onChange={handleChange}
+          value={fromInput.confirmPassword ?? ''}
+        />
+      </div>
+      {error && <p className='text-red-500 text-sm'>{error}</p>}
+      <button type='submit' className={buttonsStyles}>
+        Submit
+      </button>
     </form>
   </div>
 };
